0c36341e1a750313cb74ef0bb0b4d986482a6f63
durgaprasadkothapalli <durgaprasadkothapalli908@gmail.com>
2024-09-21 12:08:36 +0530

    terraform

---
+++ expense-1/variables.tf
@@ -0,0 +1,20 @@
+variable "instance_names"{
+    type = list(string)
+    default = [ "mysql","backend","frontend" ]
+    }
+
+    # variable "common_tags" {
+    #     type = map
+    #     default = {
+    #         Project = "expenses"
+    #          Environment = "dev"
+    #          Terraform = "true"
+    #          }
+      #}
+      variable "zone_id"{
+        default = "Z09594872R3YKNFLE7JEU"
+      }
+
+      variable "domain_names" {
+              default = "76sdaws.tech"
+      }
